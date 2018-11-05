71e2f9511ed63b13375bc27575fb005136d940c4
jonathanjm <jonathanjm84@gmail.com>
2018-11-05 13:44:29 -0500

    added aion logo

--- src/AccountUnavailable.jsx
+++ src/AccountUnavailable.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 const ErrorTemplate = require('./ErrorTemplate');
 
 const AccountUnavailable = ErrorTemplate.bind(null, {
-  title: 'No ETH Account Available',
+  title: 'No AION Account Available',
   message: `
 It seems that you don&apos;t have an AION account selected. If using
 AIWA, please make sure that your wallet is unlocked and that
